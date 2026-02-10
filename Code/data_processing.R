e7d72d409bf499f1ff1420fe0127b0de58df5ff3
Ulrich Baron-Fournier <ulrich.baron-fournier@polymtl.ca>
2026-02-10 21:08:48 +0100

    Add

--- Code/data_processing.R
+++ Code/data_processing.R
@@ -2,17 +2,19 @@ library("here")
 library("xts")
 library("zoo")
 
-load(here("Data/raw", "indices.rda"))
+load(here("Data", "raw", "indices.rda"))
 head(prices)
 tail(prices)
 summary(prices)
 plot(prices)
 range(index(prices))
 
-prices <- prices["2005-01/"]
-head(prices)
-tail(prices)
-summary(prices)
-plot(prices)
-range(index(prices))
+prices_processed <- prices["2005-01/"]
+head(prices_processed)
+tail(prices_processed)
+summary(prices_processed)
+plot(prices_processed)
+range(index(prices_processed))
 
+save(prices_processed,
+     file = here("Data", "processed", "prices_processed.rda"))
